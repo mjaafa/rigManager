d223beac163c8042b769675e4fd8c6fccf1fa993
mohamed JAAFAR <mohamet.jaafar@gmail.com>
2018-02-01 23:11:45 +0100

    windows script adaptor

--- main.py
+++ main.py
@@ -8,12 +8,21 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 from pyvirtualdisplay import Display
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from bs4 import BeautifulSoup
+import requests
+from bs4 import BeautifulSoup
 
 class maxProfit:
     coin            = 0
-    coin2           = 1
+    coinInfos       = 1
     profitability24 = 2
     profitability   = 3
+    poolServer      = 4
+    poolPort        = 5
 
 coinsDictionnary = [ "BitcoinGold"
                     ,"Bytecoin"
@@ -39,7 +48,6 @@ coinsDictionnary = [ "BitcoinGold"
                     ,"Musicoin"
                     ,"Myriad-Groestl"
                     ,"Orbitcoin"
-                    ,"PascalLite"
                     ,"Pascalcoin"
                     ,"Pirl"
                     ,"Sia"
@@ -55,66 +63,55 @@ coinsDictionnary = [ "BitcoinGold"
                     ,"Zclassic"
                     ,"Zencash"];
 
-maxProfitInfo = ["", "", 0, 0];
-
+maxProfitInfo = ["", "", 0, 0, {}, {}];
 
 headers = { 'User-Agent' : 'Mozilla/5.0' }
 req = urllib2.Request('https://whattomine.com/coins.json', None, headers)
 url = urllib2.urlopen(req).read()
 data = json.loads(url.decode())
-#pprint(json.dumps(data, sort_keys=False))
-#pprint(data)
-#print(data['coins']['Ethereum'])
-
-#def loadProfitabilityJsonInfo(__object__, __url__):
-#    req = urllib2.Request(__url__, None, headers);
-#    urlPage = urllib2.urlopen(req).read()
-#    __object__ = json.loads(urlPage.decode());
-
+coins = data['coins'];
 
 def determinateBestProfitable(__object__, __maxProfitInfo__):
-#    __maxProfitInfo__[maxProfit.coin] = __object__['coins'][0];
-#    __maxProfitInfo__[maxProfit.profitability24] = __object__['coins'][0]['profitability24'];
 
     for coinType in coinsDictionnary:
- #       print "profitability24 : ", __maxProfitInfo__[maxProfit.profitability24]
- #       print "web profitability24 : ", __object__['coins'][coinType]['profitability24']
         if(__object__['coins'][coinType]['profitability24'] > __maxProfitInfo__[maxProfit.profitability24]):
             __maxProfitInfo__[maxProfit.profitability24] = __object__['coins'][coinType]['profitability24'];
             __maxProfitInfo__[maxProfit.coin] = coinType;
 
         if(__object__['coins'][coinType]['profitability'] > __maxProfitInfo__[maxProfit.profitability]):
             __maxProfitInfo__[maxProfit.profitability] = __object__['coins'][coinType]['profitability'];
-            __maxProfitInfo__[maxProfit.coin2] = coinType;
-
 
     print ("The max profitable cryptocurrency = ", __maxProfitInfo__[maxProfit.coin], " with profitability within : ", __maxProfitInfo__[maxProfit.profitability24]);
     print ("The max profitable cryptocurrency = ", __maxProfitInfo__[maxProfit.coin], " with profitability within : ", __maxProfitInfo__[maxProfit.profitability]);
 
 
-def bestPoolSeeker(__url__, __cryptoCoin__):
-    display = Display(visible=0, size=(800, 600))
-    display.start()
-    browser = webdriver.Firefox()
+def bestPoolSeeker(__url__, __cryptoCoin__, __maxProfitInfo__):
+    browser = webdriver.PhantomJS()
     fullUrl = __url__+__cryptoCoin__;
     browser.get(fullUrl);
     print browser.title
     table = browser.find_element_by_tag_name("tbody");
+    #print "table : ", table.text
     all_rows = table.find_elements_by_tag_name("tr")
-    print "rows One", all_rows[1].text
-    cells = all_rows[1].find_elements_by_tag_name("td")
-    print ("Coins = ", cells[0].text)
+    __maxProfitInfo__[maxProfit.coinInfos] = all_rows[1].text
+    cells = all_rows[2].find_elements_by_tag_name("td")
+    soup = BeautifulSoup(browser.page_source, "html.parser")
+    _table_ = soup.find('tr', attrs={"class": u"table-pool"})
+    rowz = _table_.findAll("td");
+    server  = _table_['data-child-server']
+    port    = _table_['data-child-port']
+    __maxProfitInfo__[maxProfit.poolServer] = server.split();
+    __maxProfitInfo__[maxProfit.poolPort]   = port.split();
     browser.quit()
-    display.stop()
 
 print ("################################")
 print ("######     RigManager     ######")
 print ("################################")
 
-
-#loadProfitabilityJsonInfo(data, "https://whattomine.com/coins.json");
-#pprint(data);
 determinateBestProfitable(data, maxProfitInfo);
 url = "https://investoon.com/mining_pools/";
-bestPoolSeeker(url, "eth");
-#print len(data['coins'])
+bestPoolSeeker(url, "eth", maxProfitInfo);
+print "Servers :  ", maxProfitInfo[maxProfit.poolServer], " | ", maxProfitInfo[maxProfit.poolPort]
+print "infos server and max profit "
+pprint(maxProfitInfo)
+
